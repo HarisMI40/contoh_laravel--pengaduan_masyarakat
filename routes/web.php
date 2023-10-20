1caeba2e292df956dfb2658bc3531fcf8058720d
HarisMI40 <harismaulanaikhsan40@gmail.com>
2023-10-20 22:07:39 +0700

    + multi login untuk petugas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PengaduanController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LoginPetugasController;
+use App\Http\Controllers\PetugasController;
 use App\Http\Controllers\AuthController;
 
 
@@ -16,15 +18,35 @@ Route::get('/update-pengaduan/{id}', [PengaduanController::class, "edit"]);
 Route::get('/about/{id}', [pengaduanController::class, 'tampil_about']);
 
 Route::get('/pengaduan/{id}', [pengaduanController::class, 'detail_pengaduan']);
+
+
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
-Route::get('/logout', [LoginController::class, 'logout']);
+
+Route::get("/register", [AuthController::class, 'register']);
+Route::post("/register", [AuthController::class, 'store']);
+
+Route::get('petugas/login', [LoginPetugasController::class, 'index']);
+Route::post('petugas/login', [LoginPetugasController::class, 'login']);
+
+
+
+
+
+
+
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/home', [PengaduanController::class, 'index']);
     Route::get('/isi-pengaduan', [PengaduanController::class, 'tampil_pengaduan']);
     Route::post('/isi-pengaduan', [PengaduanController::class, 'proses_tambah_pengaduan']);
+
+    Route::get('/logout', [LoginController::class, 'logout']);
 });
 
-Route::get("/register", [AuthController::class, 'register']);
-Route::post("/register", [AuthController::class, 'store']);
+Route::middleware(['cek_petugas'])->group(function () {
+    Route::get('/petugas/home', [PetugasController::class, "index"]);
+
+  
+    Route::get('petugas/logout', [LoginPetugasController::class, "logout"]);
+});
